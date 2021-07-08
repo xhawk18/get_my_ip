7a4f32d6ee29098ef7b20aca1f767a64b849a58d
X-Hawk <xhawk18@gmail.com>
2021-07-08 17:50:15 +0800

    updated

--- git.js
+++ git.js
@@ -4,23 +4,51 @@ module.exports = $;
 let childProcess = require('child_process');
 let util = require('util');
 
-$.pull = async function(){
+$.pull = async function() {
     let ret = await util.promisify(childProcess.execFile)('git', ['pull', '--quiet']);
     console.log(ret);
 }
 
-$.pushAll = async function(){
+$.pushAll = async function() {
     let ret = await util.promisify(childProcess.execFile)('git', ['push', '--all', '--force']);
     console.log(ret);
 }
 
-$.add = async function(file){
+$.pushAllRemotes = async function(remotes) {
+    for(let remote of remotes) {
+        for(let i = 0; i < 3; ++i) {
+            try {
+                console.log(`git push --all --force ${remote}`);
+                let ret = await util.promisify(childProcess.execFile)('git', ['push', '--all', '--force', remote]);
+                console.log(ret);
+                break;
+            } catch(err) {
+                await util.promisify(setTimeout)(3000);
+            }
+        }
+    }
+}
+
+$.add = async function(file) {
     let ret = await util.promisify(childProcess.execFile)('git', ['add', file]);
     console.log(ret);
 }
 
-$.commit = async function(message){
+$.commit = async function(message) {
     let ret = await util.promisify(childProcess.execFile)('git', ['commit', '--allow-empty-message', '-m', message]);
     console.log(ret);
 }
 
+$.remote = async function() {
+    let ret = await util.promisify(childProcess.execFile)('git', ['remote']);
+    console.log(ret);
+    let remotes = [];
+    if(ret.stdout.length > 0) {
+        let arr = ret.stdout.split('\n');
+        arr.map(function(s) {
+            s = s.trim();
+            if(s.length > 0) remotes.push(s);
+        });
+    }
+    return remotes;
+}
--- main.js
+++ main.js
@@ -9,9 +9,9 @@ let os = require('os');
 const IPS_JSON = path.join(__dirname, 'ips.json');
 
 async function getMyIp(){
-    let html = await request({url: 'http://2018.ip138.com/ic.asp'});
+    let html = await request({url: 'http://2021.ip138.com/'});
     let $ = cheerio.load(html);
-    let text = $('body center').text();
+    let text = $('body p[align="center"]').text();
     if(text){
         let m = text.match(/\[([\d\.]+)\]/);
         if(m){
@@ -53,14 +53,15 @@ async function main(){
     let oldIps = await readOldIps();
     let oldIp = (oldIps && oldIps[name]);
 
-    if(newIp !== undefined && newIp !== oldIp){
+    if(newIp !== undefined && newIp !== oldIp) {
         await writeIp(name, newIp);
         console.log(`new IP ${newIp} has been updated`);
 
         try{
             await git.add(IPS_JSON);
             await git.commit('');
-            await git.pushAll();
+            let remotes = await git.remote();
+            await git.pushAllRemotes(remotes);
             console.log(`new IP ${newIp} has been committed`);
         } catch(err){
             await writeIp(name, oldIp);
@@ -68,8 +69,9 @@ async function main(){
             console.error(err);
         }
     }
-    else
+    else {
         console.log(`new IP ${newIp} not updated`);
+    }
 }
 
 main();
