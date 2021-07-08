485583f0ff2dadc6d7ec3cb68143986d458c5dc1
X-Hawk <xhawk18@gmail.com>
2021-07-08 18:46:23 +0800

    use markdown

--- main.js
+++ main.js
@@ -3,10 +3,11 @@ let path = require('path');
 let fs = require('fs');
 let request = require('./request');
 let cheerio = require('cheerio');
+let marked = require('marked');
 let git = require('./git');
 let os = require('os');
 
-const IPS_JSON = path.join(__dirname, 'ips.json');
+const IPS_MD = path.join(__dirname, 'Readme.md');
 
 async function getMyIp(){
     let html = await request({url: 'http://2021.ip138.com/'});
@@ -22,8 +23,21 @@ async function getMyIp(){
 
 async function readOldIps(){
     try{
-        let json = await util.promisify(fs.readFile)(IPS_JSON, 'utf8');
-        json = JSON.parse(json);
+        let json = {};
+        let md = await util.promisify(fs.readFile)(IPS_MD, 'utf8');
+        let html = marked(md);
+        console.log(html);
+        let $ = cheerio.load(html);
+        $('table tbody tr').each(function(i, tr) {
+            let tds = $(tr).children('td');
+            if(tds.length === 2) {
+                let name = $(tds[0]).text().trim();
+                let ip = $(tds[1]).text().trim();
+                console.log(`${ip} = ${name}`);
+                if(name.length > 0 && ip.length > 0)
+                    json[name] = ip;
+            }
+        });
         return json;
     }catch(err){
     }
@@ -37,10 +51,39 @@ async function writeIp(name, ip){
             delete json[name];
         else
             json[name] = ip;
-        json = JSON.stringify(json, '', 4);
-        await util.promisify(fs.writeFile)(IPS_JSON, json);
+        //json = JSON.stringify(json, '', 4);
+        //await util.promisify(fs.writeFile)(IPS_MD, json);
+        await writeMd(json);
     }catch(err){
     }
+}
+
+async function writeMd(json) {
+    function pad(s,p,len) {
+        if(s.length >= len) return s;
+        while(s.length < len)
+            s += p;
+        return s.substr(0, len);
+    }
+    
+    let maxNameLen = 0;
+    let maxIpLen = 0;
+    for(let name in json) {
+        let ip = json[name];
+        if(maxNameLen < name.length)
+            maxNameLen = name.length;
+        if(maxIpLen < ip.length)
+            maxIpLen = ip.length;
+    }
+
+    var md = `|${pad(""," ",maxNameLen)}|${pad(""," ",maxIpLen)}|\n`;
+    md += `|${pad("","-",maxNameLen)}|${pad("","-",maxIpLen)}|\n`;
+    for(let name in json) {
+        let ip = json[name];
+        md += `|${pad(name," ",maxNameLen)}|${pad(ip," ",maxIpLen)}|\n`;
+    }
+    
+    await util.promisify(fs.writeFile)(IPS_MD, md);
 }
 
 async function main(){
@@ -58,7 +101,7 @@ async function main(){
         console.log(`new IP ${newIp} has been updated`);
 
         try{
-            await git.add(IPS_JSON);
+            await git.add(IPS_MD);
             await git.commit('');
             let remotes = await git.remote();
             await git.pushAllRemotes(remotes);
